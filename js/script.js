24f2a1a3b09589de741ad1a7cf2a41f7f37495ad
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-23 19:32:52 +0100

    Ejercicio resuelto

--- js/script.js
+++ js/script.js
@@ -1,49 +1,57 @@
 let formulario = document.querySelector("form");
 let i = 0;
+
+function mostrarDatos() {
+  let personas = localStorage.getItem("persona") || "[]";
+  /* if (!Array.isArray(persona)) {
+    persona = []; // Si no es un array, inicialízalo como un array vacío
+  }*/
+  let array = personas.split("},{").map(String);
+  let contactos = document.getElementById("contactos");
+  return array.forEach((item) => {
+    contactos.innerHTML += `<div class="texto">
+              <p>${item.replaceAll(`","`, " ").replaceAll(`[{`, "").replaceAll("}]" , "").replaceAll(`"`, "")}</p>
+          </div>`;
+
+    i++;
+    
+  });
+}
+
+function enviarDatos(formData) {
+  let usuarios = localStorage.getItem("persona") || "[]"; // Si no hay datos, usa '[]'
+  usuarios = JSON.parse(usuarios);
+  usuarios.push(formData);
+
+  // Guardar el array actualizado en localStorage
+  return localStorage.setItem("persona", JSON.stringify(usuarios));
+}
+function borrarContacto(index) {
+  let contactos = JSON.parse(localStorage.getItem('persona')) || [];
+  contactos.splice(index, 1);
+  localStorage.setItem('persona', JSON.stringify(contactos));
+  
+}
+
+
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
-
   const formData = {
     name: document.getElementById("name").value,
     url: document.getElementById("url").value,
     email: document.getElementById("email").value,
     mensaje: document.getElementById("message").value,
   };
-  let usuarios = localStorage.getItem("persona") || "[]"; // Si no hay datos, usa '[]'
-  usuarios = JSON.parse(usuarios);
-  usuarios.push(formData);
-
-  // Guardar el array actualizado en localStorage
-  localStorage.setItem("persona", JSON.stringify(usuarios));
-
-  let personas = localStorage.getItem("persona") || "[]";
-  /* if (!Array.isArray(personas)) {
-    personas = []; // Si no es un array, inicialízalo como un array vacío
-  }*/
- document.querySelector("form").addEventListener("click", (element) => {
-  function borrarUno(element) {
-    const index = array.indexOf(element);
-    if (index > -1) {
-      // only splice array when item is found
-      array.splice(index, 1); // 2nd parameter means remove one item only
-    }
-  }
-  });
-  let array = personas.split("},{").map(String);
-  let contactos = document.getElementById("contactos");
-  array.forEach((item) => {
-    contactos.innerHTML += `<div class="texto">
-              <p>${item}</p><button type="click" id="singleDel${i}">Eliminar</button>
-          </div>`;
-
-    i++;
-    
-  });
+  enviarDatos(formData);
+  mostrarDatos();
 });
 
 //BORRAR Unico
 
+document.getElementById('botonBorrar').addEventListener('click', function() {
+  borrarContacto(parseInt(prompt("Introduce el indice de la fila a borrar, comenzado por el 0"))); 
+});
 //BORRAR TODOS
 document
   .querySelector("form")
-  .addEventListener("reset", () => localStorage.clear());
+  .addEventListener("reset", () => localStorage.clear() && mostrarDatos());
